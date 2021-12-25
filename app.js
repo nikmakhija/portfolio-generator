f7659b2823dc587d064f082cfff3bd1b01ad8883
nikmakhija <90490335+nikmakhija@users.noreply.github.com>
2021-12-24 23:41:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var commandLineArgs = proccess.argv;
 console.log(commandLineArgs);
+var profileDataArgs = proccess.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+console.log(profileDataArgs);
