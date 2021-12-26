67adda624e35a57ad381feee0f63120d27d4b362
nikmakhija <90490335+nikmakhija@users.noreply.github.com>
2021-12-26 18:25:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const profileDataArgs = proccess.argv.slice(2, proccess.argv.length);
-console.log(profileDataArgs);
-const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr);
-};
-printProfileData(profileDataArgs);
-// Notice the lack of parantheses around the 'profileDataArr' parmeter?
-const printProfileData = profileDataArr = > {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i];
-                }
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+  });
 };
