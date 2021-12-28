c1f353fd42824e9aae887b8be6e48c94c4c681a1
nikmakhija <90490335+nikmakhija@users.noreply.github.com>
2021-12-28 00:05:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,27 @@ const printProfileData = profileDataArr => {
     console.log(profileItem)
   });
 };
-const generatePage = () => Name: Nikhil, Github: nikmakhija';
-console.log(generatePage('Nikhil','nikmakhija'));
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+fs.writefile('index.html', generatePage(name, github), error => {
+  if (err) throw err;
+  console.log('Portfolio complete! Check out index.html to see the output!')
 cost generatePage = (userName, githubName) => {
   return '
   Name: ${nikmakhija}
